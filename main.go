47c80fd20b718853e02b2e174f8d05fd88e0c181
hihimamuLab <hihimamu14@outlook.jp>
2023-01-31 09:31:01 +0900

    add commentout

--- main.go
+++ main.go
@@ -9,35 +9,35 @@ import (
 )
 
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
+	scanner := bufio.NewScanner(os.Stdin) //文字入力
 	scanner.Scan()
-	equation := scanner.Text()
-	slice := strings.Split(equation, " ")
-	var num1 *int = nil
+	equation := scanner.Text()            //入力した文書を変数に代入
+	slice := strings.Split(equation, " ") //その変数をスペースで区切って新しい変数に代入
+	var num1 *int = nil                   //nilが入った空の変数の作成
 	var num2 *int = nil
-	var operand string = ""
-	for i, str := range slice {
-		if i%2 == 0 {
-			number, err := strconv.Atoi(str)
-			if err != nil {
+	var operand string = ""     //演算子を入れる空の変数の作成
+	for i, str := range slice { //slice変数から何番目の要素かと要素の内容を取得してスライスの数だけ繰り返す
+		if i%2 == 0 { //要素の番目が偶数(数値)の場合の処理
+			number, err := strconv.Atoi(str) //要素の内容を数値に変換するのとエラーハンドリングのためのerr変数の作成と代入
+			if err != nil {                  //エラーが起きた場合の処理
 				fmt.Println("数値を入力してください")
-				return
+				return //処理終了
 			}
-			num1 = num2
-			num2 = &number
-		} else {
-			switch str {
-			case "+":
+			num1 = num2    //num2をnum1に代入(値の保存)
+			num2 = &number //numberのポインタをnum2に代入
+		} else { //要素の番目が奇数(演算子)の場合の処理
+			switch str { //str(要素の内容)を引数にする
+			case "+": //和だったときの処理
 				operand = "+"
-			case "-":
+			case "-": //差だったときの処理
 				operand = "-"
-			case "*":
+			case "*": //積だったときの処理
 				operand = "*"
-			case "/":
+			case "/": //商だったときの処理
 				operand = "/"
-			default:
+			default: //例外処理
 				fmt.Println("正しい記号を入力してください")
-				return
+				return //処理終了
 			}
 		}
 		if num1 != nil && num2 != nil {
